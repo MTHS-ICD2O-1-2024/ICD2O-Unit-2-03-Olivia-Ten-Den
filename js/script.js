f7c71cabf0afb4f85d029bcd6d48ec527b357584
default <olivia.ten-den@stu.ocsb.ca>
2025-03-17 17:24:16 +0000

    updated js file

--- js/script.js
+++ js/script.js
@@ -4,9 +4,15 @@
 // Created on: Mar 17, 2025
 // This file contains the JS functions for index.html
 
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * Do basic math.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+  // output
+  document.getElementById("user-address").innerHTML =
+    "Your address is: " + streetName + ", " + streetNumber + "."
 }
