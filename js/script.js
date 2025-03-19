f4ffbb111e10c80531056d142304455762dc7eae
default <olivia.ten-den@stu.ocsb.ca>
2025-03-19 02:08:56 +0000

    switched number and name

--- js/script.js
+++ js/script.js
@@ -4,15 +4,14 @@
 // Created on: Mar 17, 2025
 // This file contains the JS functions for index.html
 
-
 /**
  * Do basic math.
  */
 function enterClicked() {
   // input
-  const streetName = document.getElementById("street-name").value
   const streetNumber = parseInt(document.getElementById("street-number").value)
+  const streetName = document.getElementById("street-name").value
   // output
   document.getElementById("user-address").innerHTML =
-    "Your address is: " + streetName + ", " + streetNumber + "."
+    "<p>The address is: " + streetNumber + " " + streetName + ".</p>"
 }
